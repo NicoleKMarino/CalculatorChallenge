3bc25821b65feb7bd93b520aded2d4dfb7208e4c
NicoleKMarino <nicolekristenmarino@gmail.com>
2025-01-11 21:36:26 -0700

    initial file seup

---
+++ CalculatorTest/CalcTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalcTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
