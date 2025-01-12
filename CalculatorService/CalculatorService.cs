cac62176b8942efe7b0a9e0789d928a59e23c2df
NicoleKMarino <nicolekristenmarino@gmail.com>
2025-01-11 22:45:50 -0700

    fulfilled and test req 1

---
+++ CalculatorService/CalculatorService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorService
+{
+    public class Calculator
+    {
+        public int Add(string inputString)
+        {
+            string[] inputs = ParseString(inputString);
+            int total = 0;
+
+            if (inputs.Length > 2) throw new Exception("Cannot calculate more than two numbers");
+
+            foreach (var number in inputs)
+            {
+                int i = int.TryParse(number, out i) ? i : 0;
+                total += i;
+            }
+
+            return total;
+        }
+
+        public String[] ParseString (string inputString)
+        {
+            return inputString.Split(',');
+        }
+    }
+}
---
+++ CalculatorTest/CalculatorTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CalculatorService;
+
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private CalculatorService.Calculator _calculator;
+
+        public CalculatorTests()
+        {
+            _calculator = new CalculatorService.Calculator();
+        }
+
+        [TestMethod]
+        public void Add_And_Return_20()
+        {
+            string input = "20";
+
+            var result = _calculator.Add(input);
+            int answer = 20;
+
+            Assert.AreEqual(answer, result);
+        }
+
+        [TestMethod]
+        public void Add_1_And_500_Return_501()
+        {
+            string input = "1,500";
+
+            var result = _calculator.Add(input);
+            int answer = 501;
+
+            Assert.AreEqual(answer, result);
+        }
+
+        [TestMethod]
+        public void Add_4_Minuss_3_Return_1()
+        {
+            string input = "4,-3";
+
+            var result = _calculator.Add(input);
+            int answer = 1;
+
+            Assert.AreEqual(answer, result);
+        }
+
+        [TestMethod]
+        public void ThrowExceptionMoreThanTwoNumbers()
+        {
+            string input = "4,3,7";
+
+            Assert.ThrowsException<Exception>(() => _calculator.Add(input));
+
+        }
+
+        [TestMethod]
+        public void Add_Empty_Convert_Zero()
+        {
+            string input = "";
+
+            var result = _calculator.Add(input);
+            int answer = 0;
+
+            Assert.AreEqual(answer, result);
+        }
+
+        [TestMethod]
+        public void Add_MissingNumbers_ConvertZero()
+        {
+            string input = "100,";
+
+            var result = _calculator.Add(input);
+            int answer = 100;
+
+            Assert.AreEqual(answer, result);
+        }
+
+        [TestMethod]
+        public void Add_InvalidNumbers_Convert_Zero()
+        {
+            string input = "5,tyty";
+
+            var result = _calculator.Add(input);
+            int answer = 5;
+
+            Assert.AreEqual(answer, result);
+        }
+    }
+}
