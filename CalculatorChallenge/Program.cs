e3396160bae755e08b5d4983bc524ca0d55ee8a2
NicoleKMarino <nicolekristenmarino@gmail.com>
2025-01-13 02:51:29 -0700

    added console app functionality

--- CalculatorChallenge/Program.cs
+++ CalculatorChallenge/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CalculatorService;
+using System;
+
+namespace R365_Calculator_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator _calculatorService = new Calculator();
+
+            Console.WriteLine("Enter Numbers");
+            string input = Console.ReadLine();
+            int result = _calculatorService.Add(input);
+            Console.WriteLine("Result: " + result);
+            Console.ReadLine();
+        }
+    }
+}
